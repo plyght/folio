59465e2374a61236e2178161a4136d7af808d728
plyght <nico.c.jaffer@gmail.com>
2023-12-18 19:00:08 -0500

    test

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,29 +1,29 @@
-'use client'
+"use client";
 
-import { FC } from 'react'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
-import { motion } from 'framer-motion'
-import styles from '@css/common.module.css'
+import { FC } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import styles from "@css/common.module.css";
 
-type Items = { [index: string]: Item }
+type Items = { [index: string]: Item };
 
 type Item = {
-  name: string
-  x: number
-  w: string
-}
+  name: string;
+  x: number;
+  w: string;
+};
 
 const items: Items = {
-  '/': { name: 'Home', x: 0, w: '77.95px' },
-  '/about': { name: 'About', x: 77.95, w: '78.48px' },
-  '/writing': { name: 'Writing', x: 156.43, w: '85.33px' },
-}
+  "/": { name: "Home", x: 0, w: "77.95px" },
+  "/about": { name: "About", x: 77.95, w: "78.48px" },
+  // '/writing': { name: 'Writing', x: 156.43, w: '85.33px' },
+};
 
 const Nav: FC = () => {
-  let pathname = usePathname() || '/'
-  if (pathname.includes('/writing/')) pathname = '/writing'
-  else if (pathname === '/404' || pathname === '/_not-found') pathname = '/'
+  let pathname = usePathname() || "/";
+  if (pathname.includes("/writing/")) pathname = "/writing";
+  else if (pathname === "/404" || pathname === "/_not-found") pathname = "/";
 
   return (
     <nav className={styles.nav}>
@@ -35,9 +35,9 @@ const Nav: FC = () => {
             {items[pathname].name === name && (
               <motion.div
                 className={styles.bar}
-                layoutId='bar'
+                layoutId="bar"
                 aria-hidden={true}
-                transition={{ type: 'spring', stiffness: 350, damping: 30 }}
+                transition={{ type: "spring", stiffness: 350, damping: 30 }}
                 style={{ width: w }}
               />
             )}
@@ -45,7 +45,7 @@ const Nav: FC = () => {
         ))}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
