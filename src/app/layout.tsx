bd9d26c91838e09a11135ee0b4a3c76ce539ff59
plyght <nico.c.jaffer@gmail.com>
2023-12-18 19:45:47 -0500

    yes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,51 @@
-import '@css/globals.css'
-import type { Metadata } from 'next'
-import { Root } from '@components/Layout'
-import AnalyticsWrapper from '@components/.../analytics'
-import Nav from '@components/Nav'
-import Footer from '@components/Footer'
-import BackToTop from '@components/BackToTop'
-import localFont from 'next/font/local'
-import clsx from 'clsx'
+import "@css/globals.css";
+import type { Metadata } from "next";
+import { Root } from "@components/Layout";
+import AnalyticsWrapper from "@components/.../analytics";
+import Nav from "@components/Nav";
+import Footer from "@components/Footer";
+import BackToTop from "@components/BackToTop";
+import localFont from "next/font/local";
+import clsx from "clsx";
 
 const inter = localFont({
-  src: './fonts/inter.woff2',
-  weight: '700',
-  variable: '--font-display',
-  display: 'swap',
-})
+  src: "./fonts/inter.woff2",
+  weight: "700",
+  variable: "--font-display",
+  display: "swap",
+});
 
 const sohne = localFont({
-  src: './fonts/sohne.woff2',
-  weight: '400',
-  variable: '--font-sans',
-  display: 'swap',
-})
+  src: "./fonts/sohne.woff2",
+  weight: "400",
+  variable: "--font-sans",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: { default: 'Harsh Singh', template: '%s — Harsh Singh' },
-  creator: 'Harsh Singh',
-  publisher: 'Harsh Singh',
-  description: 'Building tools and polished user interactions.',
-  keywords: ['Harsh Singh', 'harshhhdev'],
-  authors: [{ name: 'Harsh Singh', url: 'https://harshsingh.xyz' }],
-  colorScheme: 'dark light',
+  title: { default: "plyght", template: "%s — plyght" },
+  creator: "plyght",
+  publisher: "plyght",
+  description: "Building tools and polished user interactions.",
+  keywords: ["plyght", "plyght"],
+  authors: [{ name: "plyght", url: "https://plyght.github.io" }],
+  colorScheme: "dark light",
   openGraph: {
-    title: 'Harsh Singh',
-    description: 'Building tools and polished user interactions.',
-    url: 'https://harshsingh.xyz',
-    siteName: 'Harsh Singh',
+    title: "plyght",
+    description: "Building tools and polished user interactions.",
+    url: "https://plyght.github.io",
+    siteName: "plyght",
     images: [
-      { url: 'https://harshsingh.xyz/og.png', width: 1200, height: 630 },
+      { url: "https://harshsingh.xyz/og.png", width: 1200, height: 630 },
       {
-        url: 'https://harshsingh.xyz/og-alt.png',
+        url: "https://harshsingh.xyz/og-alt.png",
         width: 1200,
         height: 630,
         alt: 'A white background with the text "Harsh Singh" in the center',
       },
     ],
-    locale: 'en-US',
-    type: 'website',
+    locale: "en-US",
+    type: "website",
   },
   robots: {
     index: true,
@@ -54,59 +54,59 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   icons: {
-    icon: '/favicons/favicon.png',
-    shortcut: '/favicons/shortcut.png',
+    icon: "/favicons/favicon.png",
+    shortcut: "/favicons/shortcut.png",
     apple: [
-      { url: '/favicons/apple-touch-icon.png' },
+      { url: "/favicons/apple-touch-icon.png" },
       {
-        url: '/favicons/apple-icon-x3.png',
-        sizes: '180x180',
-        type: 'image/png',
+        url: "/favicons/apple-icon-x3.png",
+        sizes: "180x180",
+        type: "image/png",
       },
     ],
     other: {
-      rel: 'apple-touch-icon-precomposed',
-      url: '/favicons/apple-touch-icon-precomposed.png',
+      rel: "apple-touch-icon-precomposed",
+      url: "/favicons/apple-touch-icon-precomposed.png",
     },
   },
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#E5E5E5' },
-    { media: '(prefers-color-scheme: dark)', color: '#121212' },
+    { media: "(prefers-color-scheme: light)", color: "#E5E5E5" },
+    { media: "(prefers-color-scheme: dark)", color: "#121212" },
   ],
   twitter: {
-    card: 'summary_large_image',
-    title: 'Harsh Singh',
-    description: 'Building tools and polished user interactions.',
-    siteId: 'harshhhdev',
-    creator: '@harshhhdev',
-    creatorId: 'harshhhdev',
+    card: "summary_large_image",
+    title: "Harsh Singh",
+    description: "Building tools and polished user interactions.",
+    siteId: "harshhhdev",
+    creator: "@harshhhdev",
+    creatorId: "harshhhdev",
     images: {
-      url: 'https://harshsingh.xyz/og.png',
+      url: "https://harshsingh.xyz/og.png",
       alt: 'A black background with noise and "Harsh Singh" written across the center',
     },
   },
   verification: {
-    google: 'VWhTtgTikPqvWIY4n2rlUj6Fe9YgkfFMEET3TM7Rce0',
-    yandex: 'cfc27cbb03eb0a9c',
-    yahoo: 'yahoo',
-    other: { me: ['hi.harsh@pm.me'] },
+    google: "VWhTtgTikPqvWIY4n2rlUj6Fe9YgkfFMEET3TM7Rce0",
+    yandex: "cfc27cbb03eb0a9c",
+    yahoo: "yahoo",
+    other: { me: ["hi.harsh@pm.me"] },
   },
   alternates: {
-    canonical: 'https://harshsingh.xyz',
-    types: { 'application/rss+xml': 'https://harshsingh.xyz/rss.xml' },
+    canonical: "https://harshsingh.xyz",
+    types: { "application/rss+xml": "https://harshsingh.xyz/rss.xml" },
   },
-  assets: ['https://harshsingh.xyz/assets.zip'],
-  category: 'technology',
-}
+  assets: ["https://harshsingh.xyz/assets.zip"],
+  category: "technology",
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
-  <html lang='en' className={clsx(inter.variable, sohne.variable)}>
+  <html lang="en" className={clsx(inter.variable, sohne.variable)}>
     <body>
       <Nav />
       <Root>{children}</Root>
@@ -115,6 +115,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
       <AnalyticsWrapper />
     </body>
   </html>
-)
+);
 
-export default RootLayout
+export default RootLayout;
